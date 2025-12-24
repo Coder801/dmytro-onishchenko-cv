24929ca5e59fb7b59ffc34bd6914f11f39d46cb6
Dmytro Onishchenko <donischenko801@gmail.com>
2025-12-24 16:51:40 +0200

    Prepare for deploy

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@
 
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  basePath: isProd ? "/<repository-name>" : "",
-  assetPrefix: isProd ? "/<repository-name>/" : "",
   turbopack: {
     rules: {
       "*.svg": {
